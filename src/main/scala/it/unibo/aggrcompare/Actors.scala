fea95c6e573440377f73fdb2d5ca2fc96cc7e5d8
Roberto Casadei <robyonrails@gmail.com>
2021-08-16 17:32:43 +0200

    Gradient done with actors

--- src/main/scala/it/unibo/aggrcompare/Actors.scala
+++ src/main/scala/it/unibo/aggrcompare/Actors.scala
@@ -3,6 +3,7 @@ package it.unibo.aggrcompare
 import akka.actor.typed.scaladsl.AskPattern.Askable
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.util.Timeout
 import it.unibo.scafi.space.Point3D
 
 import scala.concurrent.duration.DurationInt
@@ -15,19 +16,28 @@ object Gradient {
   sealed trait Msg
   case class SetSource(isSource: Boolean) extends Msg
   case class SetPosition(point: Point3D) extends Msg
+  case class GetPosition(replyTo: ActorRef[NbrPos]) extends Msg
   case class SetDistance(distance: Double, from: ActorRef[Msg]) extends Msg
-  case class ComputeGradient(gradient: Double, from: ActorRef[Msg]) extends Msg
-  case class QueryGradient(replyTo: ActorRef[Double]) extends Msg
+  case object ComputeGradient extends Msg
+  case class QueryGradient(replyTo: ActorRef[NbrGradient]) extends Msg
+  case class SetNeighbourGradient(distance: Double, from: ActorRef[Msg]) extends Msg
   case class AddNeighbour(nbr: ActorRef[Msg]) extends Msg
   case class RemoveNeighbour(nbr: ActorRef[Msg]) extends Msg
   case object Round extends Msg
+
+  case class NbrPos(position: Point3D, nbr: ActorRef[Msg])
+  case class NbrGradient(gradient: Double, nbr: ActorRef[Msg])
 
   def apply(source: Boolean,
             gradient: Double,
             nbrs: Set[ActorRef[Msg]],
             distances: Map[ActorRef[Msg],Double],
             nbrGradients: Map[ActorRef[Msg],Double],
-            position: Point3D = Point3D(0,0,0)): Behavior[Msg] = Behaviors.withTimers{ timers => Behaviors.receive { case (ctx,msg) =>
+            position: Point3D = Point3D(0,0,0)): Behavior[Msg] = Behaviors.setup{ ctx =>
+    val getPositionAdapter: ActorRef[NbrPos] = ctx.messageAdapter(m => SetDistance(m.position.distance(position), m.nbr))
+    val getGradientAdapter: ActorRef[NbrGradient] = ctx.messageAdapter(m => SetNeighbourGradient(m.gradient, m.nbr))
+
+    Behaviors.withTimers { timers => Behaviors.receive { case (ctx,msg) =>
     msg match {
       case SetSource(s) =>
         Gradient(s, 0, nbrs, distances, nbrGradients, position)
@@ -37,14 +47,21 @@ object Gradient {
         Gradient(source, gradient, nbrs - nbr, distances, nbrGradients, position)
       case SetPosition(p) =>
         Gradient(source, gradient, nbrs, distances, nbrGradients, p)
-      case ComputeGradient(g, from) => {
-        val newNbrGradients = distances + (ctx.self -> gradient) + (from -> g)
+      case GetPosition(replyTo) =>
+        replyTo ! NbrPos(position, ctx.self)
+        Behaviors.same
+      case SetDistance(d, from) =>
+        Gradient(source, gradient, nbrs, distances + (from -> d), nbrGradients, position)
+      case ComputeGradient => {
+        val newNbrGradients = nbrGradients + (ctx.self -> gradient)
 
         // Once gradient is computed, start the next round in a second
         timers.startSingleTimer(Round, 1.second)
 
+        ctx.log.info(s"${ctx.self.path.name} CONTEXT\n${distances}\n${nbrGradients}")
+
         if(source){
-          ctx.log.info(s"GRADIENT: ${ctx.self.path.name} -> ${gradient}")
+          ctx.log.info(s"GRADIENT (SOURCE): ${ctx.self.path.name} -> ${gradient}")
           Gradient(source, 0, nbrs, distances, newNbrGradients, position)
         } else {
           val minNbr = newNbrGradients.minByOption(_._2)
@@ -54,18 +71,23 @@ object Gradient {
         }
       }
       case QueryGradient(replyTo) => {
-        replyTo ! gradient
+        replyTo ! NbrGradient(gradient, ctx.self)
         Behaviors.same
       }
+      case SetNeighbourGradient(d, from) =>
+        Gradient(source, gradient, nbrs, distances, nbrGradients + (from -> d), position)
       case Round => {
-        ctx.log.info(s"ROUND!")
         nbrs.foreach(nbr => {
-          // TODO: query neighbours
+          // Query neighbour for neighbouring sensors
+          nbr ! GetPosition(getPositionAdapter)
+          // Query neighbour for application data
+          nbr ! QueryGradient(getGradientAdapter)
         })
+        timers.startSingleTimer(ComputeGradient, 1.seconds)
         Behaviors.same
       }
     }
-  } }
+  } } }
 }
 
 object Actors extends App {
@@ -83,7 +105,7 @@ object Actors extends App {
       if(d<10) map(d) ! Gradient.AddNeighbour(map(d + 1))
     })
 
-    map(1) ! Gradient.SetSource(true)
+    map(3) ! Gradient.SetSource(true)
 
     map.values.foreach(_ ! Gradient.Round)
 
