4b8aa5e4232017fa06f8182eb97a2176ee7ce032
Roberto Casadei <robyonrails@gmail.com>
2021-08-18 11:52:05 +0200

    Fourth approach: directly on algorithms, but should be hardwired

---
+++ src/main/scala/it/unibo/aggrcompare/Actors4.scala
@@ -0,0 +1,68 @@
+package it.unibo.aggrcompare
+
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
+
+/**
+ * 4th attempt: focus on algorithmic logic
+ */
+object ChannelLogic {
+  type Nbr = String
+
+  case class GradientContext (
+    val isSource: Boolean = false,
+    val neighboursGradients: Map[Nbr,Double] = Map.empty,
+    val neighboursDistances: Map[Nbr,Double] = Map.empty
+  )
+
+  trait GradientProtocol
+  case class ComputeGradient(c: GradientContext, replyTo: ActorRef[Double]) extends GradientProtocol
+
+  def gradient(): Behavior[GradientProtocol] = Behaviors.receiveMessage {
+    case ComputeGradient(c, replyTo) =>
+      val g = if(c.isSource) 0.0 else {
+        c.neighboursGradients.minByOption(_._2).map {
+          case (nbr,nbrg) => nbrg + c.neighboursDistances(nbr)
+        }.getOrElse(Double.PositiveInfinity)
+      }
+      replyTo ! g
+      Behaviors.same
+  }
+
+  trait GradientContextProtocol
+  case class SetSource(source: Boolean) extends GradientContextProtocol
+  case class SetNeighbourGradient(nbr: Nbr, gradient: Double) extends GradientContextProtocol
+  case class SetNeighbourDistance(nbr: Nbr, distance: Double) extends GradientContextProtocol
+  case class Get(replyTo: ActorRef[GradientContext])
+
+  def gradientContext(c: GradientContext): Behavior[GradientContextProtocol] = Behaviors.receiveMessage[GradientContextProtocol] {
+    case SetSource(s) => gradientContext(c.copy(isSource = s))
+    case SetNeighbourGradient(nbr, g) => gradientContext(c.copy(neighboursGradients = c.neighboursGradients + (nbr -> g)))
+    case SetNeighbourDistance(nbr, d) => gradientContext(c.copy(neighboursDistances = c.neighboursDistances + (nbr -> d)))
+    // TODO: must also consider that neighbours may be removed together their data
+  }
+
+  trait ChannelContext {
+    val distanceToSource: Double
+    val distanceToTarget: Double
+    val distanceBetweenSourceAndTarget: Double
+    val tolerance: Double
+  }
+  case class ComputeChannel(c: ChannelContext, replyTo: ActorRef[Boolean])
+
+  def channel(): Behavior[ChannelContext] = Behaviors.receiveMessage {
+    case ComputeChannel(c, replyTo) =>
+      c.distanceToSource + c.distanceToTarget <= c.distanceBetweenSourceAndTarget + c.tolerance
+      Behaviors.same
+  }
+}
+
+
+object Actors4 extends App {
+  println("Actors implementation")
+
+  val system = ActorSystem[C.Start.type](Behaviors.setup { ctx =>
+
+    Behaviors.ignore
+  }, "ActorBasedChannel")
+}
